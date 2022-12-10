f843eb56ec43a8671f758ce44da6e418429d8a5f
Omar Elsherif <omartare@buffalo.edu>
2022-12-10 15:16:38 -0500

    submit day 10 part 2

--- day10_2.py
+++ day10_2.py
@@ -2,7 +2,31 @@ f = open("inputs/10.txt", "r")
 lines = f.readlines()
 f.close()
 
+def action(pos, X):
+    global msg
+    if pos in range(X-1, X+2):
+        msg += "#"
+    else:
+        #msg += "."
+        msg += " "
+    pos = (pos+1)%40
+    return pos
+
 msg = ""
 X = 1
-count = 1
+pos = 0
 
+for line in lines:
+    words = line.split()
+    if words[0] == 'noop':
+        pos = action(pos, X)
+    elif words[0] == 'addx':
+        V = int(words[1])
+        pos = action(pos, X)
+        pos = action(pos, X)
+        X += V
+
+i = 0
+while i in range(len(msg)):
+    print(msg[i:i+40])
+    i += 40
